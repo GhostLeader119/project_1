4828828974bec8d296b039b7b478f435101d5e50
GhostLeader119 <97194393+GhostLeader119@users.noreply.github.com>
2022-02-24 11:52:28 -0700

    Add files via upload

---
+++ project_1/dice.py
@@ -0,0 +1,57 @@
+import random
+class dice():
+    #you roll a 1d6
+    
+    def __init__(self):
+        self._die = []
+        self._dice_number = 1
+        self._min_value = 1
+        self._max_value = 6
+
+
+    def roll(self):
+        self._die = []
+        for i in range(self._dice_number):
+            _result = random.randint(self._min_value,self._max_value)
+            self._die.append(_result)
+
+
+    def send_dice(self):
+
+        return self._die
+
+
+    def add_another_dice(self,amount):
+
+        self._dice_number + amount
+
+
+    def remove_a_dice(self,amount):
+
+        self._dice_number - amount
+    
+
+class enemy_dice(dice):
+    #The enemy rolls 1d12
+
+    def __init__(self):
+        self._die = []
+        self._dice_number = 1
+        self._min_value = 1
+        self._max_value = 12
+
+
+def self_test():
+    exam = dice()
+    exam.roll()
+    me = exam.send_dice()
+    expiriment = enemy_dice()
+    expiriment.roll()
+    you = expiriment.send_dice()
+
+    print(f'Me: {me} You: {you}')
+
+
+    
+
+self_test()
